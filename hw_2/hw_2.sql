2f5907335718d40e07bc52246cd259a76222dc12
Olya Tatianina <olya.tatianina@gmail.com>
2022-11-26 13:32:47 +0400

    hw_2.sql modified

--- hw_2/hw_2.sql
+++ hw_2/hw_2.sql
@@ -7,9 +7,9 @@ artist_id integer primary key,
 artist_name VARCHAR not null
 );
 create table if not exists genres_artists (
-id integer primary key,
 genre_id integer references genres(genre_id),
-artist_id integer references artists(artist_id)
+artist_id integer references artists(artist_id),
+constraint pk primary key (genre_id, artist_id)
 );
 create table if not exists albums (
 album_id integer primary key,
@@ -17,14 +17,14 @@ album_name VARCHAR not null,
 album_year integer check(album_year > 0)  not null
 );
 create table if not exists artists_albums (
-id integer primary key,
 artist_id integer references artists(artist_id),
-album_id integer references albums(album_id)
+album_id integer references albums(album_id),
+constraint pk_2 primary key (artist_id, album_id)
 );
 create table if not exists tracks (
 track_id integer primary key,
 track_name VARCHAR not null,
-track_length integer not null,
+track_length integer check(track_length > 0) not null,
 album_id integer references albums(album_id)
 );
  create table if not exists compilations (
@@ -33,9 +33,7 @@ album_id integer references albums(album_id)
  compilation_year integer check(compilation_year > 0)  not null
  );
 create table if not exists tracks_compilations (
-id integer primary key,
 track_id integer references tracks(track_id),
-compilation_id integer references compilations(compilation_id)
+compilation_id integer references compilations(compilation_id),
+constraint pk_3 primary key (track_id, compilation_id)
 );
-alter table tracks
-add check(track_length > 0);
